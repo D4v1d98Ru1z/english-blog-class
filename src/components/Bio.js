6dc676416085632452cbfe82e9b1e7be5c2a7307
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2019-02-03 20:03:20 -0500

    Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -31,12 +31,11 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              Blog created by{' '}
+              <a href="https://www.instagram.com/davidle.codes/">
+                David Ruiz
+              </a>.{' '}
+              For&nbsp;The english class.
             </p>
           </div>
         )
