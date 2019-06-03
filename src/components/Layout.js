05bfadd5a2b58049af257e21517d798aa33a84c1
David Ruiz <davidl.ruizc@gmail.com>
2019-06-02 21:25:46 -0500

    Adding a line

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -62,6 +62,7 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
+        <hr/>
         <footer>
           © {new Date().getFullYear()}, Built by
           {` `}
